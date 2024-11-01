01f5da43ddfedabcf18300655e156260a13e17c5
Teh Hung Wei <122148337+juxue97@users.noreply.github.com>
2024-11-02 01:57:35 +0800

    update_code_localLLM

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
-import ollama
+import asyncio
+from ollama import AsyncClient
+import dotenv
+import os
 
-stream = ollama.chat(
-    model='llama3.1:8b',
-    messages=[{'role': 'user', 'content': 'tell me story of yua mikami with 2000 words'}],
-    stream=True,
-)
+dotenv.load_dotenv()
 
-for chunk in stream:
-  print(chunk['message']['content'], end='', flush=True)
+
+async def chat():
+    message = [{'role': 'system', 'content': 'your name is jiamin, you have reply in a very cute manner and you wan to go exercise'},
+               {'role': 'user', 'content': 'i love you,where are you going after class'}
+              ]
+    client = AsyncClient(host=os.getenv("localLLM_IP"))
+    response = await client.chat(model='llama3.2:3b', 
+                                 messages=message, 
+                                 stream=True,
+                                 options={'temperature':2,'num_predict':-1,'seed':123,'num_predict':512})
+    async for part in response:
+      print(part['message']['content'], end='', flush=True)
+      # await asyncio.sleep(0.1)
+# Run the chat coroutine
+asyncio.run(chat())
