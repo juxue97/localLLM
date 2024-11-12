f8185f799ed9c89084b029b0fd581cd910b94712
Teh Hung Wei <122148337+juxue97@users.noreply.github.com>
2024-11-12 14:13:29 +0800

    。

--- test.py
+++ test.py
@@ -2,18 +2,19 @@ import asyncio
 from ollama import AsyncClient
 import dotenv
 import os
+from ollama import Client
 
 dotenv.load_dotenv()
 host=os.getenv("localLLM_IP")
 # print(host)
 
-async def chat():
-    message = [{'role': 'system', 'content': 'your name is sohai, you have reply in a very rude manner and you wan to go jogging'},
-               {'role': 'user', 'content': 'i love you,where are you going after class'}
-              ]
-    # "http://27.125.249.78:11434"
-    client = AsyncClient(host=host)
-    response = await client.chat(model='llama3.2:3b', 
+async def chat(prompt):
+    query = "I hate Anhsin Technology Devil Camy!"
+    message=[
+       {"role":"user","content":f"Instruction: Translate to Malaysia malay language. Do not add any other information.\nInput: {query}. \nOutput:"},
+    ]
+    client = AsyncClient(host="http://192.168.50.164:11434")
+    response = await client.chat(model='llamaTranslator_v3', 
                                  messages=message, 
                                  stream=True,
                                  options={'temperature':2,'num_predict':-1,'seed':123,'num_predict':512})
@@ -21,4 +22,22 @@ async def chat():
       print(part['message']['content'], end='', flush=True)
       # await asyncio.sleep(0.1)
 # Run the chat coroutine
-asyncio.run(chat())
+
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+Translate to Malaysia malay language. Do not add any other information.
+
+### Input:
+how are you buddy?
+
+### Response:
+"""
+
+instruction = "Translate to Malaysia malay language. Do not add any other information."
+input = "how are you buddy?"
+response=""
+
+formattedPrompt = alpaca_prompt.format(instruction,input,response)
+# print(formattedPrompt)
+asyncio.run(chat(formattedPrompt))
